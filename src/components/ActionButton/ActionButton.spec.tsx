64c6aa63ac62ccf2cc53530ceeaba4eb87495f15
Hebert Paziam <hebert.paziam>
2023-06-09 00:01:05 -0300

    test: adjusting action button tests

--- src/components/ActionButton/ActionButton.spec.tsx
+++ src/components/ActionButton/ActionButton.spec.tsx
@@ -1,17 +1,54 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import ActionButton, { ActionButtonProps } from "./ActionButton";
 import { faker } from "@faker-js/faker";
 
 describe("ActionButton", () => {
-  const props: ActionButtonProps = {
-    children: <>{faker.lorem.word()}</>,
-    type: "button",
-    disabled: faker.datatype.boolean(),
-  };
-  
+  let text: string;
+  let props: ActionButtonProps;
+
+  beforeEach(() => {
+    text = faker.lorem.word();
+    props = {
+      children: <>{text}</>,
+      type: "button",
+      disabled: faker.datatype.boolean(),
+      onClick: jest.fn(),
+    };
+  });
+
   it("should render the component", () => {
     const component = render(<ActionButton {...props} />);
 
     expect(component).toBeDefined();
+  });
+
+  it("should trigger the click event", () => {
+    render(<ActionButton {...props} disabled={false} />);
+
+    fireEvent.click(screen.getByText(text));
+
+    expect(props.onClick).toHaveBeenCalled();
+  });
+
+  it("should not trigger the click event when button is disabled", () => {
+    render(<ActionButton {...props} disabled={true} />);
+
+    fireEvent.click(screen.getByText(text));
+
+    expect(props.onClick).not.toHaveBeenCalled();
+  });
+
+  it("should trigger submit event", () => {
+    const onSubmit = jest.fn((e) => e.preventDefault());
+
+    render(
+      <form onSubmit={onSubmit}>
+        <ActionButton {...props} type={"submit"} disabled={false} />
+      </form>
+    );
+
+    fireEvent.click(screen.getByText(text));
+
+    expect(onSubmit).toHaveBeenCalled();
   });
 });
--- src/components/ActionButton/ActionButton.tsx
+++ src/components/ActionButton/ActionButton.tsx
@@ -1,21 +1,28 @@
 "use client";
 
-import React, { ReactNode } from "react";
+import React, { MouseEventHandler, ReactNode } from "react";
 import "./ActionButton.styles.scss";
 
 export type ActionButtonProps = {
   type: "submit" | "button";
   disabled?: boolean;
   children: ReactNode;
+  onClick: MouseEventHandler<HTMLButtonElement>;
 };
 
 export default function ActionButton({
   type = "button",
   disabled,
   children,
+  onClick,
 }: ActionButtonProps) {
   return (
-    <button className="action-button" type={type} disabled={disabled}>
+    <button
+      className="action-button"
+      type={type}
+      disabled={disabled}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
