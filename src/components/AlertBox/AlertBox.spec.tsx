c0ea8cfabafd976d8a6933d8d62b38e29b6021e9
Hebert Paziam <hebert.paziam>
2023-06-09 00:08:14 -0300

    test: adjusting alert box tests

--- src/components/AlertBox/AlertBox.spec.tsx
+++ src/components/AlertBox/AlertBox.spec.tsx
@@ -1,18 +1,36 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import AlertBox, { AlertBoxProps } from "./AlertBox";
 import { faker } from "@faker-js/faker";
 
 describe("AlertBox", () => {
-  const props: AlertBoxProps = {
-    response: {
-      message: faker.lorem.sentence(),
-      ok: faker.datatype.boolean(),
-    },
-  };
-  
+  let props: AlertBoxProps;
+
+  beforeEach(() => {
+    props = {
+      response: {
+        message: faker.lorem.sentence(),
+        ok: faker.datatype.boolean(),
+      },
+    };
+  });
+
   it("should render the component", () => {
     const component = render(<AlertBox {...props} />);
 
     expect(component).toBeDefined();
+  });
+
+  it("should render error alert box", () => {
+    render(<AlertBox response={{ ...props.response, ok: false }} />);
+    const alert = screen.getByText(props.response.message);
+
+    expect(alert.className).toEqual("alert-box -error");
+  });
+
+  it("should render success alert box", () => {
+    render(<AlertBox response={{ ...props.response, ok: true }} />);
+    const alert = screen.getByText(props.response.message);
+
+    expect(alert.className).toEqual("alert-box -success");
   });
 });
